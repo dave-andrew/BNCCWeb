50e754355681d64b29cc869109156ffd0c68f0d5
Dep <109551168+dave-andrew@users.noreply.github.com>
2022-12-02 16:46:49 +0700

    first commit

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-let position = "absolute"
-let navbarTop = 0
-let lastScrollPosition = 0
-
-const supportPageOffset = window.pageYOffset !== undefined
-
-function onScroll() {
-  const navbar = document.querySelector("#nav")
-  const currentScrollPosition = supportPageOffset ? window.pageYOffset : document.documentElement.scrollTop
-  if (currentScrollPosition <= 0) {
-    position = "absolute"
-    navbarTop = 0
-    lastScrollPosition = 0
-  } else {
-    if (currentScrollPosition > lastScrollPosition) {
-      position = "absolute"
-      let { top, height } = navbar.getBoundingClientRect()
-      if (top < -height) {
-        top = -height
-      }
-      navbarTop = currentScrollPosition + top
-    } else if (currentScrollPosition < lastScrollPosition) {
-      const { top } = navbar.getBoundingClientRect()
-      if (top >= 0) {
-        navbarTop = 0
-        position = "fixed"
-      }
+var scroll = 0;
+var navbars = document.getElementById("navbar");
+var upbtn = document.getElementById("upbtn");
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop>scroll){
+      navbars.style.top = "-200px";
+      upbtn.style.display = "block";
+    }else{
+      navbars.style.top = "0";
     }
-    lastScrollPosition = currentScrollPosition 
-  }
-  navbar.style = `position: ${position}; top: ${navbarTop}px;`
-}
+    if(scrollTop<scroll){
+      upbtn.style.display = 'none';
+    }
+    scroll = scrollTop;
+});
 
-window.addEventListener("scroll", onScroll, { passive: true })
+window.addEventListener('scroll', reveal);
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i=0;i<reveals.length;i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop<windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
